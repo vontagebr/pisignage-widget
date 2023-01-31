66ce2debbba1e08b58cb1647cb2f43ce44b73375
Paulo Santos <pauloramires@gmail.com>
2023-01-31 15:29:16 -0300

    current state

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="bg-red-300 h-[200px] bg-banner bg-auto flex justify-center items-end text-white">
-    <span class="p-1 text-xs">{{ date }}</span>
+    class="bg-red-300 h-[200px] bg-banner bg-auto flex justify-start items-end text-white">
+    <span class="p-1 text-xs font-bold">{{ date }}</span>
   </div>
 </template>
 
@@ -10,5 +10,5 @@ import { ref } from 'vue'
 const date = ref(new Date().toISOString())
 setInterval(() => {
   date.value = new Date().toISOString()
-}, 100)
+}, 10)
 </script>
