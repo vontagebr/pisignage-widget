95a2d0848dbda68ac8cf124b6f6bc44217bedf40
Paulo Santos <pauloramires@gmail.com>
2023-01-31 15:15:22 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="bg-red-300 h-[200px] bg-banner bg-auto flex justify-center items-end text-white">
+    <span class="p-1 text-xs">{{ date }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const date = ref(new Date().toISOString())
+setInterval(() => {
+  date.value = new Date().toISOString()
+}, 100)
+</script>
